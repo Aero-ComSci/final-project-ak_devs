0aabe690e6e37a36a4e34401bff9dded31643c74
kathartikiscooked <ksingamsetty2@gmail.com>
2025-05-27 10:34:27 -0700

    https://docs.google.com/document/d/1iBBK1576E7mIofjdB6rS8UoSgHUR3i3V-UyloodJr6Q/edit?usp=sharing

---
+++ karthikcode.py
@@ -0,0 +1,22 @@
+#karthikcode
+city_dictionary = {
+    "New York":"40.66 73.94",
+    "Los Angeles":"34.02 118.41",
+    "Chicago":"41.84 87.68",
+    "Houston":"29.79 95.39",
+    "Phoenix":"33.57 112.09",
+    "Philadelhia":"40.01 75.13",
+    "San Antonio":"29.46 98.52",
+    "San Diego":"32.81 98.52",
+    "Dallas":"32.79 96.77",
+    "Jacksonville":"30.34 81.66",
+    "Fort Worth":"32.78 97.35",
+    "San Jose":"37.30 121.81",
+    "Austin":"30.30 97.75",
+    "Charlotte":"35.21 80.83",
+    "Columbus":"39.99 82.99",
+    "Indianapolis":"39.78 86.15",
+    "San Francisco":"37.73 123.03",
+    "Seattle":"47.62 122.35",
+    "Denver":"39.76 104.88"
+}
