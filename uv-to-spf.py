92b57c884d44d2c190396814b2e5d7a53e38a305
Krishna Nidamarthi <knidam79@gmail.com>
2025-05-29 09:30:05 -0700

    Create uv-to-spf.py

---
+++ uv-to-spf.py
@@ -0,0 +1,5 @@
+uvamount_recommendedSPF = {"0-2":15,
+                           "3-5":30,
+                           "6-7":40,
+                           "8-10":50,
+                           "11+":"Stay indoors"}
